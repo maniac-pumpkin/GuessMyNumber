a084f90b2d5d156799a4ab78465b421bbd690dd5
Mohammad Mehdi Beheshti <shadymx@yahoo.com>
2023-02-25 17:24:41 +0330

    Deploying my unfinished project

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// let randomNumber = Math.ceil(Math.random() * 20);
+// const againBtn = document.querySelector(".again-btn");
+// const number = document.querySelector(".thenumber");
+// const checkerInput = document.querySelector(".checkerinput");
+// const checkBtn = document.querySelector(".checkbtn");
+// const statusText = document.querySelector(".status");
+// const score = document.querySelector(".score");
+// const highestRecord = document.querySelector(".highestrecord");
