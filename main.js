b85be635140a8d891a466ab7a0ce8799c07af2a2
Mohammad Mehdi Beheshti <shadymx@yahoo.com>
2023-02-26 10:10:22 +0330

    adding some changes to game

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 "use strict";
 
-// let randomNumber = Math.ceil(Math.random() * 20);
-// const againBtn = document.querySelector(".again-btn");
-// const number = document.querySelector(".thenumber");
-// const checkerInput = document.querySelector(".checkerinput");
-// const checkBtn = document.querySelector(".checkbtn");
-// const statusText = document.querySelector(".status");
-// const score = document.querySelector(".score");
-// const highestRecord = document.querySelector(".highestrecord");
+const startingPage = document.querySelector(".starting-page");
+const mainPage = document.querySelector(".game-main-page");
+
+let randomNumber;
+let chosenNumber;
+
+const gameStarter = (randomNum, chosenNum) => {
+	startingPage.addEventListener("submit", (e) => {
+		e.preventDefault();
+
+		const warningText = document.querySelector(".warning");
+
+		chosenNumber = startingPage.staringInput.value;
+		randomNumber = Math.ceil(Math.random() * Number(chosenNumber));
+
+		if (chosenNumber > 1 && chosenNumber <= 1000) {
+			startingPage.style.display = "none";
+			mainPage.style.display = "block";
+		} else {
+			warningText.style.visibility = "visible";
+		}
+	});
+};
