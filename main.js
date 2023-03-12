dd3a0d063ca62cb6c2c85fed5e58d7d5b4dba508
Mohammad Mehdi Beheshti <shadymx@yahoo.com>
2023-03-12 21:19:04 +0330

    Enhancing

--- main.js
+++ main.js
@@ -3,23 +3,19 @@
 const startingPage = document.querySelector(".starting-page");
 const mainPage = document.querySelector(".game-main-page");
 
-let randomNumber;
 let chosenNumber;
+let randomNumber;
 
-const gameStarter = (randomNum, chosenNum) => {
-	startingPage.addEventListener("submit", (e) => {
-		e.preventDefault();
+startingPage.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const warningText = document.querySelector(".warning");
+	chosenNumber = startingPage.staringInput.value;
+	randomNumber = Math.ceil(Math.random() * Number(chosenNumber));
 
-		const warningText = document.querySelector(".warning");
-
-		chosenNumber = startingPage.staringInput.value;
-		randomNumber = Math.ceil(Math.random() * Number(chosenNumber));
-
-		if (chosenNumber > 1 && chosenNumber <= 1000) {
-			startingPage.style.display = "none";
-			mainPage.style.display = "block";
-		} else {
-			warningText.style.visibility = "visible";
-		}
-	});
-};
+	if (chosenNumber > 20 && chosenNumber <= 1000) {
+		startingPage.style.display = "none";
+		mainPage.style.display = "block";
+	} else {
+		warningText.style.visibility = "visible";
+	}
+});
